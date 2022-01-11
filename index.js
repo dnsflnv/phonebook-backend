1cb12d51d364e6a9f3df746c061112cf43626860
Denis Filonov <denis@filonov.pro>
2022-01-11 16:47:49 +0100

    3.15

--- index.js
+++ index.js
@@ -16,68 +16,42 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static('build'));
 
-
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-];
-
 app.get('/', (request, response) => {
     response.send('<h1>Phonebook backend.</h1>');
 });
 
 app.get('/info', (request, response) => {
     let date = new Date();
-    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`);
+    Person.countDocuments({}, (error, count) => {
+        response.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`);
+    });
+
 });
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(people => {
         response.json(people);
-    })
+    });
 });
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(person => person.id === id);
-
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch(err => next(err));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter(p => p.id !== id);
-    response.status(204).end();
+// TODO: Make frontend work with it.
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => { response.status(204).end(); })
+        .catch(err => next(err));
 });
-
-const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(p => p.id))
-        : 0
-    return maxId + 1
-}
 
 app.post('/api/persons', (request, response) => {
     const body = request.body;
